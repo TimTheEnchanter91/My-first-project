63adc97da2a9bbff3b77689b0c149bee6ed8b74a
Timothy <timothy@sunstreams-MacBook-Pro.local>
2015-05-26 17:42:07 -0700

    adding files

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var people = [
+    {
+        "userName" : "Tim",
+        "phoneNumber" : "510-2222"
+    },
+    {
+        "userName" : "Zeus",
+        "phoneNumber" : "111-1111"
+    },
+    {
+        "userName" : "Richsfj",
+        "phoneNumber" : "244-2222"
+    },
+    {
+        "userName" : "boom boom",
+        "phoneNumber" : "565-4444"
+    },
+    {
+        "userName" : "Yaho Mendoza",
+        "phoneNumber" : "510-2555"
+    },
+    {
+        "userName" : "Jahsa Son",
+        "phoneNumber" : "510-6757"
+    },
+    {
+        "userName" : "Bradizzle",
+        "phoneNumber" : "510-5656"
+    },
+    {
+        "userName" : "Yapa",
+        "phoneNumber" : "510-2222"
+    }
+}
